b61e23d4d9571702221af2461a989c6f6f288d4e
vejdos <115791092+vejdos@users.noreply.github.com>
2022-10-14 14:27:04 +0300

    Add project files.

---
+++ DependencyProvider.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+
+namespace MongoDB
+{
+    public class DependencyProvider
+    {
+        public IServiceCollection ServiceCollection;
+
+        public DependencyProvider()
+        {
+            CreateServiceCollection();
+        }
+
+        private void CreateServiceCollection()
+        {
+            ServiceCollection = new ServiceCollection();
+            ServiceCollection.AddTransient(typeof(IMongoDatabaseSettings), (obj) => { return new MongoDatabaseSettings()
+            {
+                DatabaseName = "gpnoid_test",
+                ConnectionString = "mongodb://localhost:27017"
+            }; });
+            ServiceCollection.AddTransient<IMongoDataContextFactory, MongoDataContextFactory>();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using MongoDB;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+using System;
+using System.Configuration;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MongoConsole
+{
+    class Program
+    {
+        private DependencyProvider _dependencyProvider;
+
+        static void Main(string[] args)
+        {
+            string connectionString = "mongodb://localhost:27017";
+            MongoClient client = new MongoClient(connectionString);
+            GetDatabaseNames(client).GetAwaiter().GetResult();
+            //  GetDatabaseNames(client);
+            //  IMongoDatabase database = client.GetDatabase("test");
+
+        }
+        private static async Task GetDatabaseNames(MongoClient client)
+        {
+            using (var cursor = await client.ListDatabasesAsync())
+            {
+                var databaseDocuments = await cursor.ToListAsync();
+                foreach (var databaseDocument in databaseDocuments)
+                {
+                    // if (databaseDocument["name"] == "local")
+                    client.DropDatabase("Delete");
+                    client.
+
+
+                        DropDatabase
+
+                    Console.WriteLine(databaseDocument["name"]);
+
+
+
+                }
+            }
+        }
+
+
+
+
+
+    }
+}
